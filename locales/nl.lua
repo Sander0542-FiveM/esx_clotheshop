a3fd4121a5b839e5e4c7c73a268b9f98e021b8ea
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-01 18:26:23 +0200

    Add Dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['nl'] = {
+  ['valid_this_purchase'] = 'deze aankoop valideren?',
+  ['yes'] = 'ja',
+  ['no'] = 'nee',
+  ['not_enough_money'] = 'je hebt niet genoeg geld',
+  ['press_menu'] = 'druk op ~INPUT_CONTEXT~ om toegang te krijgen tot de ~y~Kleding winkel~s~.',
+  ['clothes'] = 'kleding winkel',
+  ['you_paid'] = 'je hebt ~g~€%s~s~ betaald',
+  ['save_in_dressing'] = 'wil je de outfit in je huis bewaren?',
+  ['name_outfit'] = 'noem je outfit',
+  ['saved_outfit'] = 'de outfit is opgeslagen!',
+}
